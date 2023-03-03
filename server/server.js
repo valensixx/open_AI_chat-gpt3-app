b83a283190cf36208aebca26dff9cf4859ea747d
Valen Black <valensixx@gmail.com>
2023-03-03 15:51:02 +0200

    setting up the server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,30 @@ import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY, 
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cros());
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message:'Hello from AI', 
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            
+        })
+    } catch (error) {
+        
+    }
+})
