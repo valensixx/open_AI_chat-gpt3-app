ea534448f3b1b65e1349fc3b411d4689f9c9b025
Valen Black <valensixx@gmail.com>
2023-03-05 13:47:38 +0200

    fix

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-//import bot from '../client/assets/bot.svg';
-//import user from '../client/assets/user.svg';
+import bot from './assets/bot.svg';
+import user from '/client/assets/user.svg';
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
